fa76922fd28a7975f7c5cc500549bd5beca249e3
Ilia Voronin <iliavoronin9898@gmail.com>
2017-09-15 19:47:44 +0300

    исправил ошибки в lesson 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,151 +1,142 @@
-@file:Suppress("UNUSED_PARAMETER") 
-package lesson1.task1 
+@file:Suppress("UNUSED_PARAMETER")
 
-import java.lang.Math.* 
+package lesson1.task1
 
-/** 
-* Пример 
-* 
-* Вычисление квадрата вещественного числа 
-*/ 
-fun sqr(x: Double) = x * x 
+import java.lang.Math.*
 
-/** 
-* Пример 
-* 
-* Вычисление дискриминанта квадратного уравнения 
-*/ 
-fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c 
+/**
+ * Пример
+ *
+ * Вычисление квадрата вещественного числа
+ */
+fun sqr(x: Double) = x * x
 
-/** 
-* Пример 
-* 
-* Поиск одного из корней квадратного уравнения 
-*/ 
-fun sqRoot(a: Double, b: Double, c: Double) = (-b + sqrt(discriminant(a, b, c))) / (2 * a) 
+/**
+ * Пример
+ *
+ * Вычисление дискриминанта квадратного уравнения
+ */
+fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
 
-/** 
-* Пример 
-* 
-* Поиск произведения корней квадратного уравнения 
-*/ 
+/**
+ * Пример
+ *
+ * Поиск одного из корней квадратного уравнения
+ */
+fun sqRoot(a: Double, b: Double, c: Double) = (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+
+/**
+ * Пример
+ *
+ * Поиск произведения корней квадратного уравнения
+ */
 fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
-val sd = sqrt(discriminant(a, b, c)) 
-val x1 = (-b + sd) / (2 * a) 
-val x2 = (-b - sd) / (2 * a) 
-return x1 * x2 // Результат 
-} 
+    val sd = sqrt(discriminant(a, b, c))
+    val x1 = (-b + sd) / (2 * a)
+    val x2 = (-b - sd) / (2 * a)
+    return x1 * x2 // Результат
+}
 
-/** 
-* Пример главной функции 
-*/ 
-fun main(args: Array<String>) { 
-// Решаем x^2 - 3*x + 2 = 0 
-val x1x2 = quadraticRootProduct(1.0, -3.0, 2.0) 
-println("Root product: $x1x2") 
-} 
+/**
+ * Пример главной функции
+ */
+fun main(args: Array<String>) {
+// Решаем x^2 - 3*x + 2 = 0
+    val x1x2 = quadraticRootProduct(1.0, -3.0, 2.0)
+    println("Root product: $x1x2")
+}
 
-/** 
-* Тривиальная 
-* 
-* Пользователь задает время в часах, минутах и секундах, например, 8:20:35. 
-* Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае). 
-*/ 
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int { 
-val h = hours * 3600 
-val m = minutes * 60 
+/**
+ * Тривиальная
+ *
+ * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
+ * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
+ */
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
+    val h = hours * 3600
+    val m = minutes * 60
 
-return h+m+seconds 
-} 
+    return h + m + seconds
+}
 
-/** 
-* Тривиальная 
-* 
-* Пользователь задает длину отрезка в саженях, аршинах и вершках (например, 8 саженей 2 аршина 11 вершков). 
-* Определить длину того же отрезка в метрах (в данном случае 18.98). 
-* 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см. 
-*/ 
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double { 
-val s:Double = sagenes * 2.1336 
-val a:Double = arshins * 0.7112 
-val v:Double = vershoks * 0.04445 
+/**
+ * Тривиальная
+ *
+ * Пользователь задает длину отрезка в саженях, аршинах и вершках (например, 8 саженей 2 аршина 11 вершков).
+ * Определить длину того же отрезка в метрах (в данном случае 18.98).
+ * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
+ */
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+        (sagenes * 48 + arshins * 16 + vershoks) * 4.445 / 100
 
-return s+a+v 
-} 
+/**
+ * Тривиальная
+ *
+ * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
+ * Вывести значение того же угла в радианах (например, 0.63256).
+ */
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double =
+        grad * (PI / 180) + min * (PI / (180 * 60)) + sec * (PI / (180 * 3600))
 
-/** 
-* Тривиальная 
-* 
-* Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд). 
-* Вывести значение того же угла в радианах (например, 0.63256). 
-*/ 
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double { 
-val g:Double = grad * 0.0174533 
-val m:Double = min * 0.000290888 
-val s:Double = sec * 4.84814e-6 
+/**
+ * Тривиальная
+ *
+ * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
+ * Например, расстояние между (3, 0) и (0, 4) равно 5
+ */
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+    val a = pow(x2 - x1, 2.0)
+    val b = pow(y2 - y1, 2.0)
 
-return g+m+s 
-} 
+    return sqrt(a + b)
+}
 
-/** 
-* Тривиальная 
-* 
-* Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2). 
-* Например, расстояние между (3, 0) и (0, 4) равно 5 
-*/ 
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double { 
-val a:Double = pow(x2-x1,2.0) 
-val b:Double = pow(y2-y1,2.0) 
+/**
+ * Простая
+ *
+ * Пользователь задает целое число, большее 100 (например, 3801).
+ * Определить третью цифру справа в этом числе (в данном случае 8).
+ */
+fun thirdDigit(number: Int): Int = number / 100 % 10
 
-return sqrt(a+b) 
-} 
+/**
+ * Простая
+ *
+ * Поезд вышел со станции отправления в h1 часов m1 минут (например в 9:25) и
+ * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
+ * Определите время поезда в пути в минутах (в данном случае 216).
+ */
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val dep = hoursDepart * 60 + minutesDepart
+    val arr = hoursArrive * 60 + minutesArrive
 
-/** 
-* Простая 
-* 
-* Пользователь задает целое число, большее 100 (например, 3801). 
-* Определить третью цифру справа в этом числе (в данном случае 8). 
-*/ 
-fun thirdDigit(number: Int): Int = number / 100 % 10 
+    return arr - dep
+}
 
-/** 
-* Простая 
-* 
-* Поезд вышел со станции отправления в h1 часов m1 минут (например в 9:25) и 
-* прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01). 
-* Определите время поезда в пути в минутах (в данном случае 216). 
-*/ 
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int { 
-val dep = hoursDepart * 60 + minutesDepart 
-val arr = hoursArrive * 60 + minutesArrive 
+/**
+ * Простая
+ *
+ * Человек положил в банк сумму в s рублей под p% годовых (проценты начисляются в конце года).
+ * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
+ * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
+ */
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val mult = (1 + percent / 100.0)
+    val ans = initial * pow(mult, 3.0)
 
-return arr-dep 
-} 
+    return ans
+}
 
-/** 
-* Простая 
-* 
-* Человек положил в банк сумму в s рублей под p% годовых (проценты начисляются в конце года). 
-* Сколько денег будет на счету через 3 года (с учётом сложных процентов)? 
-* Например, 100 рублей под 10% годовых превратятся в 133.1 рубля 
-*/ 
-fun accountInThreeYears(initial: Int, percent: Int): Double { 
-val mult:Double = (1+percent.toDouble()/100) 
-val ans:Double = (((initial*mult)*mult)*mult) 
+/**
+ * Простая
+ *
+ * Пользователь задает целое трехзначное число (например, 478).
+ *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
+ */
+fun numberRevert(number: Int): Int {
+    val a = number / 100
+    val b = number / 10 % 10
+    val c = number % 10
 
-return ans 
-} 
-
-/** 
-* Простая 
-* 
-* Пользователь задает целое трехзначное число (например, 478). 
-*Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874). 
-*/ 
-fun numberRevert(number: Int): Int { 
-val a = number / 100 
-val b = number / 10 % 10 
-val c = number % 10 
-
-return (c*100 + b*10 + a) 
+    return (c * 100 + b * 10 + a)
 }
